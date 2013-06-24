2740f2328c8d2dbfebec33121282b114734f02a9
Zhen Yang <nwu.scott119@gmail.com>
2013-06-24 23:59:33 +0800

    first commit: set up project

---
+++ src/test/java/com/tw/AppTest.java
@@ -0,0 +1,16 @@
+package com.tw;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testName() throws Exception {
+        assertThat(true, Is.is(true));
+    }
+}
